adb4df06412df56183faacb9fea47a732871e0d4
GangOfFour199 <willwaight394@gmail.com>
2024-12-11 15:31:23 +0000

    Update capitalise.js

--- Scripts/capitalise.js
+++ Scripts/capitalise.js
@@ -1,4 +1,4 @@
-// A capitalise function that takes a string and returns it with the first character capitalized.
+// A capitalise function that takes a string and returns it with the first character capitalised.
 
 function capitalise(word) {
   return String(word).charAt(0).toUpperCase() + String(word).slice(1);
